e0348d5a522bc464f6d97bef7e291bcd9b28c461
neil <neil@gmail.com>
2025-02-07 16:10:09 -0500

    cambios-ssl

--- validators/incidencia/createIncidenciaValidator.js
+++ validators/incidencia/createIncidenciaValidator.js
@@ -6,11 +6,9 @@ const validateIncidencia = {
     .isInt()
     .withMessage("Unidad id debe ser un número entero"),
   tipo_caso_id: body("tipo_caso_id")
-    .optional()
     .isInt()
     .withMessage("Tipo caso id debe ser un número entero"),
   sub_tipo_caso_id: body("sub_tipo_caso_id")
-    .optional()
     .isInt()
     .withMessage("Sub tipo caso id debe ser un número entero"),
   tipo_reportante_id: body("tipo_reportante_id")
@@ -27,7 +25,6 @@ const validateIncidencia = {
     .isInt()
     .withMessage("Cargo sereno id debe ser un número entero"),
   nombre_reportante: body("nombre_reportante")
-    .optional()
     .isString()
     .withMessage("Nombre reportante debe ser un string"),
   sereno_id: body("sereno_id")
@@ -60,11 +57,9 @@ const validateIncidencia = {
     .isInt()
     .withMessage("Jurisdiccion id debe ser un número entero"),
   fecha_registro: body("fecha_registro")
-    .optional()
     .isDate()
     .withMessage("Fecha registro debe ser una fecha válida"),
   hora_registro: body("hora_registro")
-    .optional()
     .matches(/^([0-9]|1[0-9]|2[0-3]):[0-5][0-9](?::[0-5][0-9])?$/)
     .withMessage(
       "Hora registro debe estar en formato H:mm, HH:mm, H:mm:ss o HH:mm:ss",
@@ -78,34 +73,24 @@ const validateIncidencia = {
       "Hora ocurrencia debe estar en formato H:mm, HH:mm, H:mm:ss o HH:mm:ss",
     ),
   estado_proceso_id: body("estado_proceso_id")
-    .optional()
     .isInt()
     .withMessage("Estado proceso id debe ser un número entero"),
-  estado_agresor_id: body("estado_agresor_id")
-    .optional()
-    .isInt()
-    .withMessage("Estado agresor id debe ser un número entero"),
   genero_agresor_id: body("genero_agresor_id")
-    .optional()
     .isInt()
     .withMessage("Genero agresor id debe ser un número entero"),
   genero_victima_id: body("genero_victima_id")
-    .optional()
     .isInt()
     .withMessage("Genero victima id debe ser un número entero"),
   severidad_proceso_id: body("severidad_proceso_id")
-    .optional()
     .isInt()
     .withMessage("Severidad proceso id debe ser un número entero"),
   severidad_id: body("severidad_id")
-    .optional()
     .isInt()
     .withMessage("Severidad id debe ser un número entero"),
   medio_id: body("medio_id")
     .isInt()
     .withMessage("Medio id debe ser un número entero"),
   medio_reporte_id: body("medio_reporte_id")
-    .optional()
     .isInt()
     .withMessage("Medio reporte id debe ser un número entero"),
   situacion_id: body("situacion_id")
@@ -113,7 +98,6 @@ const validateIncidencia = {
     .isInt()
     .withMessage("Situacion id debe ser un número entero"),
   operador_id: body("operador_id")
-    .optional()
     .isInt()
     .withMessage("Operador id debe ser un número entero"),
   motivo: body("motivo").isString().withMessage("Motivo debe ser un texto"),
