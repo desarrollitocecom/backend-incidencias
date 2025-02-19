204a1b91a0e37fe61e3e9a448a855101fcfee9b2
neil <neil@gmail.com>
2025-02-19 10:42:18 -0500

    cambios-comentario

--- controllers/incidenciasController.js
+++ controllers/incidenciasController.js
@@ -59,25 +59,24 @@ const postIncidencia = async (incidencia) => {
       }
     });
 
-    // const { data, status } = await axios.post(
-    //   `${INCIDENCIAS_URL}/api/crear_incidencia`,
-    //   formData,
-    //   {
-    //     headers: {
-    //       ...formData.getHeaders(),
-    //     },
-    //   },
-    // );
+    const { data, status } = await axios.post(
+      `${INCIDENCIAS_URL}/api/crear_incidencia`,
+      formData,
+      {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      },
+    );
 
-    // if (data.success !== true) throw new Error("Error al crear la incidencia");
+    if (data.success !== true) throw new Error("Error al crear la incidencia");
 
-    // incidenciaData.estado = "APROBADO";
-    // incidenciaData.convertidaAIncidencia = true;
-    // incidenciaData.codigo_incidencia = data.codigo_incidencia;
+    incidenciaData.estado = "APROBADO";
+    incidenciaData.convertidaAIncidencia = true;
+    incidenciaData.codigo_incidencia = data.codigo_incidencia;
 
-    // const preincidencia = await updatePreIncidencia(id, incidenciaData);
-    // return data;
-    return 'ok';
+    const preincidencia = await updatePreIncidencia(id, incidenciaData);
+    return data;
   } catch (error) {
     console.log("Error: ", error);
     throw error;
